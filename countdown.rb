e415a2b44c584bfac916b576eab76036c9b530d5
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-05-09 20:57:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(number)
   
   while number > 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
-end
-"HAPPY NEW YEAR!"
+    puts "#{number} SECOND(S)!"
+    number -= 1
+ end
+ "HAPPY NEW YEAR!"
 end
